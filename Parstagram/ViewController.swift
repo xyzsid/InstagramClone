b86c67bb322376b8b152d029059b819166c46d2a
xyzsid <sid.dommeti@gmail.com>
2021-10-07 23:41:48 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Sid on 10/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
